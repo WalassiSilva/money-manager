875a6cfc66ad29597f2728ae728c73e0987e709d
Walassi Silva <walassi.b@gmail.com>
2024-01-27 07:02:43 -0300

    add route getall transactions

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/api/getall", (req, res) => {
-  res.send("Get all transactions");
+app.get("/api/getall", async (req, res) => {
+  const getall = await prisma.transaction.findMany();
+  res.json(getall);
 });
 
 app.listen(port, () => console.log(`Running in http://localhost:${port}`));
