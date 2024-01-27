7a4e7a7e89cb4c829cb6c228800d0b84397bd185
Walassi Silva <walassi.b@gmail.com>
2024-01-27 12:19:53 -0300

    add post transaction  route

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,12 +4,14 @@ import { PrismaClient } from "@prisma/client";
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
+app.use(express.json());
 
-app.get("/api/getall", async (req, res) => {
+//--------- GET ALL TRANSACTIONS-----------
+app.get("/api/getall", async (_, res) => {
   const getall = await prisma.transaction.findMany({
     orderBy: {
       day: "desc",
-    }, 
+    },
     include: {
       categories: true,
     }
@@ -17,5 +19,28 @@ app.get("/api/getall", async (req, res) => {
   });
   res.json(getall);
 });
+
+// -------- POST NEW TRANSACTION -----------
+app.post("/api/post", async (req, res) => {
+  const { title, value, category_id, day, type } = req.body;
+
+  try {
+    await prisma.transaction.create({
+      data: {
+        title,
+        value,
+        category_id,
+        type,
+        day: new Date(day)
+      }
+    });
+  } catch (error) {
+    return res.status(500).send({ message: "Erro ao cadastrar uma transação" });
+  }
+
+  res.status(201).send();
+});
+
+
 
 app.listen(port, () => console.log(`Running in http://localhost:${port}`));
