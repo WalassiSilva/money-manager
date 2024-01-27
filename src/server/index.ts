5787132e7df9e5eb13425760377bbc360914bf17
Walassi Silva <walassi.b@gmail.com>
2024-01-27 06:02:26 -0300

    update eslint and first express route

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,8 +3,8 @@ import express from "express";
 const port = 3000;
 const app = express();
 
-app.get("/api/", (req, res) => {
-  res.send("Hello wolrd!");
+app.get("/api/getall", (req, res) => {
+  res.send("Get all transactions");
 });
 
-app.listen(port, () => console.log("Running in http://localhost:3000"));
+app.listen(port, () => console.log(`Running in http://localhost:${port}`));
