482e3a7a3488b4f04117d9e897dc9cb8dba3a112
Walassi Silva <walassi.b@gmail.com>
2024-03-10 00:09:59 -0300

    change to external server
    separate the backend from the project

--- src/variables.tsx
+++ src/variables.tsx
@@ -1,5 +1,5 @@
+const baseUrl = "https://money-maneger-backend.onrender.com/api/transactions";
 // const baseUrl = "http://localhost:3001/api/transactions";
-const baseUrl = "https://money-manager-two.vercel.app/api/transactions";
 // const baseUrl = "https://money-manager-6mbb.onrender.com";
 
 export { baseUrl };
