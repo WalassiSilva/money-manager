36a6f9b0a53359a9abbc312e761f5a9405f315c6
Walassi Silva <91761745+WalassiSilva@users.noreply.github.com>
2025-01-01 15:26:39 +0000

    feat: add buton to header's calendar

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,11 +1,11 @@
-
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { useDateContext } from "../../context/GlobalProvider";
+import { FaCalendarAlt } from "react-icons/fa";
 
 const Header = () => {
-
+  const [showCalendar, setShowCalendar] = useState(false);
   const { date, setDate } = useDateContext();
 
   useEffect(() => {
@@ -17,14 +17,23 @@ const Header = () => {
   }
 
   return (
-    <div className="text-black py-1 flex justify-center items-center ">
-      <Calendar
-        onClickMonth={e => onChangeHandle(e)}
-        value={date}
-        view="year"
-        className={"w-56 p-0 h-10 overflow-hidden duration-300 hover:duration-300 hover:h-[250px] rounded-lg bg-gray-500 text-white"}
-
-      />
+    <div className="text-black py-1 flex flex-col justify-center items-center gap-4 transaction">
+      <button
+        className="text-white p-5 bg-white/50 rounded-full"
+        onClick={() => setShowCalendar(!showCalendar)}
+      >
+        <FaCalendarAlt />
+      </button>
+      {showCalendar && (
+        <Calendar
+          onClickMonth={(e) => onChangeHandle(e)}
+          value={date}
+          view="year"
+          className={
+            "w-56 p-0 overflow-hidden duration-300 hover:duration-300 rounded-lg bg-gray-500 text-white"
+          }
+        />
+      )}
     </div>
   );
 };
