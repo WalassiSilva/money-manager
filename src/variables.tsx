65e5e93d979aa4d8de9f00dfecc2cf32fae235cc
Walassi Silva <walassi.b@gmail.com>
2025-03-09 22:05:39 -0300

    fix: update baseURL

--- src/variables.tsx
+++ src/variables.tsx
@@ -1,5 +1,4 @@
-// const baseUrl = "https://api-money-manager.onrender.com/api/transactions";
-const baseUrl = "http://localhost:3333/api/transactions";
-// const baseUrl = "https://money-manager-6mbb.onrender.com";
+const baseUrl = "https://api-money-manager.onrender.com/api/transactions";
+// const baseUrl = "http://localhost:3333/api/transactions";
 
 export { baseUrl };
