dd20cf51c42975df7d667b5bf7ca5f53b355b6be
Walassi Silva <walassi.b@gmail.com>
2026-05-03 10:43:51 -0300

    style: lower Card size.

--- src/components/Transactions/TransactionsCard/index.tsx
+++ src/components/Transactions/TransactionsCard/index.tsx
@@ -10,23 +10,27 @@ export const TransactionsCard: React.FC<TransactionsProps> = ({
   type,
 }) => {
   return (
-    <section className="bg-gray-600 m-4 p-4 rounded-xl flex hover:shadow-white hover:translate-x-2 duration-300">
+    <section className="bg-gray-600 m-4 px-4 py-1 rounded-xl flex hover:shadow-white hover:translate-x-2 duration-300">
       <div className="flex items-center justify-center m-3 ml-0">
         {setIconCategory(Number(category_id))}
       </div>
 
-      <div className="flex-1 ">
-        <h2 className="font-bold text-sm md:text-lg capitalize">{title}</h2>
-        <div className="flex justify-between mt-2">
-          <div>
-            <p className="text-xs md:text-lg">{formatDate(day)}</p>
-          </div>
+      <div className="flex-1 flex justify-between ">
+        <h2 className="font-bold self-center text-sm md:text-lg capitalize">
+          {title}
+        </h2>
+
+        {/*value and day info */}
+        <div className=" flex flex-col justify-between mt-2">
           <div
             className={`${
               type !== 0 ? "text-green-300" : "text-red-500"
             } font-bold`}
           >
             {monetaryValue(value)}
+          </div>
+          <div>
+            <p className="text-xs text-end">{formatDate(day)}</p>
           </div>
         </div>
       </div>
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -43,66 +43,60 @@ export const setIconCategory = (category_id: number) => {
   switch (category_id) {
     case 1: {
       return (
-        <FaHome className={"bg-[#2c3d57] red-500 rounded-full p-2 h-10 w-10"} />
+        <FaHome className={"bg-[#2c3d57] red-500 rounded-full p-2 h-8 w-8"} />
       );
     }
     case 2: {
       return (
-        <GiPlantWatering className="bg-[#115e59] rounded-full p-2 h-10 w-10" />
+        <GiPlantWatering className="bg-[#115e59] rounded-full p-2 h-8 w-8" />
       );
     }
     case 3: {
       return (
-        <GiFireSpellCast className="bg-red-500 rounded-full p-2 h-10 w-10" />
+        <GiFireSpellCast className="bg-red-500 rounded-full p-2 h-8 w-8" />
       );
     }
     case 4: {
       return (
-        <MdHealthAndSafety className="bg-[#d97706] rounded-full p-2 h-10 w-10" />
+        <MdHealthAndSafety className="bg-[#d97706] rounded-full p-2 h-8 w-8" />
       );
     }
     case 5: {
-      return <FaBus className="bg-black rounded-full p-2 h-10 w-10" />;
+      return <FaBus className="bg-black rounded-full p-2 h-8 w-8" />;
     }
     case 6: {
-      return <IoBuild className="bg-[#422006] rounded-full p-2 h-10 w-10" />;
+      return <IoBuild className="bg-[#422006] rounded-full p-2 h-8 w-8" />;
     }
     case 7: {
       return (
-        <GiReceiveMoney className="bg-[#065f46] rounded-full p-2 h-10 w-10" />
+        <GiReceiveMoney className="bg-[#065f46] rounded-full p-2 h-8 w-8" />
       );
     }
     case 8: {
       return (
         <FaChartLine
           size={2}
-          className="bg-[#7842f5] rounded-full p-2 h-10 w-10"
+          className="bg-[#7842f5] rounded-full p-2 h-8 w-8"
         />
       );
     }
     case 9: {
-      return (
-        <GiMoneyStack className="bg-[#54816c] rounded-full p-1 h-10 w-10" />
-      );
+      return <GiMoneyStack className="bg-[#54816c] rounded-full p-1 h-8 w-8" />;
     }
     case 10: {
-      return (
-        <GiMoneyStack className="bg-green-700 rounded-full p-2 h-10 w-10" />
-      );
+      return <GiMoneyStack className="bg-green-700 rounded-full p-2 h-8 w-8" />;
     }
     case 11: {
-      return (
-        <GiStairsGoal className="bg-blue-500 rounded-full p-2 h-10 w-10" />
-      );
+      return <GiStairsGoal className="bg-blue-500 rounded-full p-2 h-8 w-8" />;
     }
     case 12: {
       return (
-        <IoFastFoodSharp className="bg-red-700 rounded-full p-2 h-10 w-10" />
+        <IoFastFoodSharp className="bg-red-700 rounded-full p-2 h-8 w-8" />
       );
     }
     case 13: {
       return (
-        <IoFastFoodSharp className="bg-yellow-600 rounded-full p-2 h-10 w-10" />
+        <IoFastFoodSharp className="bg-yellow-600 rounded-full p-2 h-8 w-8" />
       );
     }
   }
